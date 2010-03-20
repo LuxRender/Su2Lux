15fae4b1404251bb859c61a034cb5210b8e56c5a
lfrisken <devnull@localhost>
2010-03-21 04:14:26 +1100

    more work on LuxTypes almost in a usable state now! still a lot more to do to take full advantage of this system.
    that's it for tonight for me!

--- su2lux/LuxrenderTypes.rb
+++ su2lux/LuxrenderTypes.rb
@@ -1,7 +1,7 @@
 ######## -- Basic Types -- ############
 class LuxType
   attr_reader :type_str, :name
-  def export(luxtype_object)
+  def export(luxtype_object)#todo: add another argument with tab level or use a singleton
     obj = luxtype_object
     #of the form:
     #"bool directsampleall" ["true"]
@@ -15,12 +15,22 @@ class LuxNumber < LuxType
   def export(luxnumber_object)
     super(luxnumber_object)
   end
+  #type conversion
+  def to_f
+    @value.to_f
+  end
+  def to_i
+    @value.to_i
+  end
+  def to_s
+    @value.to_s
+  end
 end
 
 class LuxInt < LuxNumber
   def initialize(name, value=0)
     @name = name
-    @value = n.to_i #add check if integer or number, convert if other number.
+    @value = value.to_i #add check if integer or number, convert if other number.
     @type_str = "integer"
   end
   def export
@@ -31,7 +41,7 @@ end
 class LuxFloat  < LuxNumber
   def initialize(name, value=0.0)
     @name = name
-    @value = n.to_f
+    @value = value.to_f
     @type_str = "float"
   end
   def export
@@ -40,13 +50,25 @@ class LuxFloat  < LuxNumber
 end
 
 class LuxBool < LuxType
+  attr_accessor :value
   def initialize(name, value)
     @name = name
     @value = value #todo: add check for boolean
     @type_str = "bool"
   end
   def export
-    super(self)
+    return "\"#{@type_str} #{@name}\" [\"#{@value.to_s}\"]"
+  end
+  #type conversion
+  def to_s
+    @value.to_s
+  end
+  def to_i
+    if @value == true
+      return 1
+    else
+      return 0
+    end
   end
 end
 
@@ -59,10 +81,17 @@ class LuxVector < LuxType
     @z = vector_array[2]
     @type_str = "vector"
   end
-  def export()
+  def export
     #of the form:
     #"vector sundir" [-0.423824071310226 -0.772439983723215 0.472979521886207]
     return "\"#{@type_str} #{@name}\" [#{@x.to_s} #{@y.to_s} #{@z.to_s}]"
+  end
+  #type conversion
+  def to_s
+    return "[#{@x}, #{@y}, #{@z}]"
+  end
+  def to_a
+    return [@x, @y, @z]
   end
 end
 
@@ -74,13 +103,106 @@ class LuxString < LuxType
   attr_accessor :value
 end
 
+###### -- groupings -- ######
+class LuxObject
+  attr_reader :name, :elements
+  def initialize(name, *elements)
+    @name = name
+    @elements = elements #array of elements
+  end
+  def export
+    export_str = @name
+    for e in @elements
+      export_str += " " + e.export #watch out for spaces with lux importing
+    end
+    return export_str
+  end
+end
 
-######## -- Settings Secific -- #########
+######## -- Settings/export Secific -- #########
 # generate types with ui updating properties?
 
 class LuxSelection
-  #usefull for lightgroup and lightsource and many others
+  #usefull for value in lightgroup and lightsource and many others
+  attr_reader :name, :choices
+  attr_accessor :selection, :children
+  def initialize(name, choices=[],default_choice=0)
+    @name = name
+    @choices = choices
+    @selection = choices[default_choice]
+  end
+  def add_choice(choice)
+    @choices.push(choice)
+  end
+  def create_choice(name, children=[])
+    @choices.push(LuxChoice.new(name, children))
+    if not @selection
+      @selection = choices[0]
+    end
+  end
+  def export
+    export_str = ""
+    for choice in choices
+      export_str += " " + choice.export + "\n\t\t"#todo: proper formatting
+    end
+    return export_str
+  end
+  #type conversion
+  def to_s
+    @selection
+  end
+  def to_a
+    @choices
+  end
+  #accessibility
+  def select(new_selection)
+    if @choices.include?(new_selection)
+      @selection = new_selection
+    else
+      #raise some kind of error
+    end
+  end
 end
+
+class LuxChoice
+  attr_reader :name
+  attr_accessor :children
+  def initialize(name, children=[])
+    @name = name
+    if children.is_a?(Array) == false #allow simple creation of choice with one child
+      children = [children]
+    end
+    @children = children #array
+  end
+  def add_child(child)
+    @children.push(child)
+  end
+  def export
+    export_str = "\"#{@name.to_s}\""
+    for child in children
+      export_str += " " + child.export
+    end
+    return export_str
+  end
+  def to_s
+    return @name
+  end
+end
+
+class Attribute
+end
+
 
 ######## -- Material Specific -- ########
 
+####### -- experimentation area -- #######
+
+prop1 = LuxFloat.new('xwidth', 2)
+prop2 = LuxFloat.new('ywidth', 2)
+
+sel_menu = LuxSelection.new('Selection')
+sel_menu.create_choice('mitchell', [prop1, prop2])
+
+obj = LuxObject.new('PixelFilter', sel_menu)
+
+print obj.export
