af2ccab591673435629be621bc029a43c85295f0
Abel Groenewolt <devnull@localhost>
2014-12-27 22:50:28 -0700

    updated release date to today

--- su2lux/su2lux.rb
+++ su2lux/su2lux.rb
@@ -34,7 +34,7 @@ module SU2LUX
 
     # Module constants
     SU2LUX_VERSION = "0.44rc"
-    SU2LUX_DATE = "23 November 2014" # to be updated in about.html manually
+    SU2LUX_DATE = "27 December 2014" # to be updated in about.html manually
 	DEBUG = true
 	FRONT_FACE_MATERIAL = "SU2LUX Front Face"
 	PLUGIN_FOLDER = "su2lux"
